dcd734cc8c7eb93210fa244cba870944dcd1141b
BL-Abhisheak <abhisheak.bhaskaran@bridgelabz.com>
2026-06-18 15:59:52 +0530

    Initial Commit

---
+++ src/main/java/com/bookmystay/bookmystayapp/BookmystayappApplication.java
@@ -0,0 +1,13 @@
+package com.bookmystay.bookmystayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmystayappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmystayappApplication.class, args);
+	}
+
+}
